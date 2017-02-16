50931770245a0f42661768f3c3e2c551fdab1188
Bartosz Kropidłowski <dev.croppek@gmail.com>
2017-02-17 00:58:05 +0100

    v0.55

--- js/system.js
+++ js/system.js
@@ -95,26 +95,19 @@ $(document).ready(function(){
                 
             });
 
-            switch(category) 
-            {
-                case 'devices':
+            $.post("php/kategorie/"+category+".php", {give_headlines: true}, function(data){
 
-                    $.post("php/kategorie/devices.php", {give_headlines: true}, function(data){
-                        
-                        setTimeout(function(){
-                            
-                            $('#modal_addtodb_content').empty().append(data);
-                            
-                            $('#add_item_to_db_form').submit(function(){
-                                return false;
-                            });
-                            
-                        }, 50);
+                setTimeout(function(){
 
+                    $('#modal_addtodb_content').empty().append(data);
+
+                    $('#add_item_to_db_form').submit(function(){
+                        return false;
                     });
 
-                    break;
-            }
+                }, 50);
+            });
+
         }
         
         setTimeout(function(){$("#add_todb_category").removeAttr("disabled")}, 1500);
@@ -129,6 +122,7 @@ $(document).ready(function(){
         
         switch(category)
         {
+            //#################################################### TUTAJ DODAWAĆ NOWE KATEGORIE ########################################################
             case 'devices':
                 
                 var id = $('#item_id_holder').text();
@@ -138,23 +132,23 @@ $(document).ready(function(){
                 var damaged = $('input[name=damaged]:checked').val();
                 var comment = $('#adddb_comment_input').val();
                 
+                if(id != '' && name != '' && type != null && damaged != '')
+                {
+                    $.post("php/kategorie/devices.php", {id: id, name: name, placement: placement, type: type, damaged: damaged, comment: comment}, function(data){
+
+                        if(data == 'success')
+                        {
+                            location.reload();
+                        }
+                        else
+                        {
+                            alert(data);
+                        }
+
+                    });
+                }
+                
                 break;
-        }
-        
-        if(id != '' && name != '' && type != null && damaged != '')
-        {
-            $.post("php/kategorie/devices.php", {id: id, name: name, placement: placement, type: type, damaged: damaged, comment: comment}, function(data){
-
-                if(data == 'success')
-                {
-                    location.reload();
-                }
-                else
-                {
-                    alert(data);
-                }
-
-            });
         }
         
         setTimeout(function(){$("#add_item_to_db").removeAttr("disabled")}, 1500);
@@ -170,8 +164,9 @@ $(document).ready(function(){
         if(confirm("Czy na pewno chcesz usunąć ten przedmiot z bazy danych?\n\nAre you sure you want to remove this item from the database?"))
         {
             var id = $('#item_id_holder').text();
+            var category = $('#item_category_holder').text();
             
-            $.post("php/kategorie/devices.php", {remove_item: id}, function(data){
+            $.post("php/kategorie/"+ category +".php", {remove_item: id}, function(data){
 
                 if(data == 'success')
                 {
@@ -196,8 +191,9 @@ $(document).ready(function(){
         if(confirm("Czy na pewno chcesz usunąć ten komentarz?\n\nAre you sure you want to remove this comment?"))
         {
             var id = $(this).parent().parent().children(':first-child').text();
+            var category = $(this).parent().parent().children(':first-child').next().text();
             
-            $.post("php/kategorie/devices.php", {remove_comment: id}, function(data){
+            $.post("php/kategorie/"+ category +".php", {remove_comment: id}, function(data){
 
                 if(data == 'success')
                 {
@@ -247,23 +243,137 @@ $(document).ready(function(){
 
             element.css({'position': 'fixed', 'left': current_position_left, 'top': current_position_top, 'z-index': '10', 'width': def_width});
             $('.panel').css({'-webkit-box-shadow': '0px 0px 10px 0px rgba(255,255,255,1)', '-moz-box-shadow': '0px 0px 10px 0px rgba(255,255,255,1)', 'box-shadow': '0px 0px 10px 0px rgba(255,255,255,1)'});
-            
-            //dopisać przycisk zamknięcia oraz potwierdzenia zmian / dodać backend
-            element_content.html('<input style="width: 100%;" type="text" value="'+current_content+'">');
+                    
+            $.post("php/kategorie/"+category+".php", {item_header: header, current_content: current_content}, function(data){
 
-            //to jest opcja zamknięcia okienka
-            setTimeout(function(){
+                element_content.html(data);
+
+            });
+            
+            //zamknięcie trybu edycji
+            $(element_content).on('click', '#close_edit_btn', function(){
                 
                 element.css({'position': def_position, 'left': def_left, 'top': def_top, 'z-index': def_zindex, 'width': ''});
                 $('.panel').css({'-webkit-box-shadow': '', '-moz-box-shadow': '', 'box-shadow': ''});
                 element_content.text(current_content);
                 $('#page_blend').fadeOut(500);
                 
-                setTimeout(function(){$(".edit_item_info_btn").removeAttr("disabled")}, 1500);
+                setTimeout(function(){$(".edit_item_info_btn").removeAttr("disabled")}, 500);
+                
+            });
+            
+            //potwierdzenie edycji i zapisanie nowych danych do bazy danych
+            $(element_content).on('click', '#confirm_edit_btn', function(){
+                
+                var new_value = $('#new_value').val();
+                
+                if(new_value === undefined)
+                {
+                    new_value = $('input[name=new_value]:checked').val();
+                }
+                
+                if(new_value == '')
+                {
+                    $('#error_alert').fadeIn(500);
+                }
+                else
+                {
+                    var id = $('#item_id_holder').text();
+                    
+                    $('#error_alert').fadeOut(500);
+                            
+                    $.post("php/kategorie/"+category+".php", {item_id: id, item_header: header, new_value: new_value}, function(data){
 
-            }, 3000); 
+                        if(data == 'success')
+                        {
+                            location.reload();
+                        }
+                        else
+                        {
+                            alert(data);
+                        }
+
+                    });
+                       
+                }
+                
+            });
             
         });
+        
+    });
+    
+    //#####################################################################
+    
+    //obsługa przycisku dodania komentarza do przedmiotu
+    var inform_specialist = false;
+    
+    $("#comment_checkbox").change(function() 
+    {
+        if(this.checked)
+        {
+            inform_specialist = true;
+            $('#notification_settings').slideDown(100);
+        }
+        else
+        {
+            inform_specialist = false;
+            $('#notification_settings').slideUp(100);
+        }
+    });
+    
+    $('#add_comment_to_db').on('click', function(){
+        
+        $(this).attr("disabled", "disabled");
+        
+        var category = $('#item_category_holder').text();
+        var id = $('#item_id_holder').text();
+        var send_notification = false;
+        var specialization = null;
+        
+        var full_url = window.location.protocol + "//" + window.location.host + "" + window.location.pathname + '?id=' + id;
+        
+        var comment = $('#new_comment_input').val();
+        
+        if(comment == '')
+        {  
+            $('#error_alert').fadeIn(500);
+        }
+        else
+        {
+            $('#error_alert').fadeOut(500);
+            
+            if(inform_specialist == true)
+            {
+                specialization = $('#specialization_select').val();
+
+                if(specialization == 'brak')
+                {
+                    $('#specialization_select').css({'outline': '#ea7676 solid thin', 'background-color': '#f2dede'});
+                }
+                else
+                {
+                    $('#specialization_select').css({'outline': '', 'background-color': ''});
+                    
+                    send_notification = true;
+                }
+            }
+            
+            $.post("php/kategorie/"+category+".php", {item_id: id, notification: send_notification, specialization: specialization, new_comment: comment, fullurl: full_url}, function(data){
+
+                if(data == 'success')
+                {
+                    location.reload();
+                }
+                else
+                {
+                    alert(data);
+                }
+
+            });
+        }
+        
+        setTimeout(function(){$("#add_comment_to_db").removeAttr("disabled")}, 1500);
         
     });
     
